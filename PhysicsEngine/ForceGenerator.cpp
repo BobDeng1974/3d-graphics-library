910a3d1f2e22f764589d2db40728c41a7332d6a7
lele94218 <terry.trxue@gmail.com>
2017-12-13 00:20:34 -0500

    add plane simulator

---
+++ PhysicsEngine/ForceGenerator.cpp
@@ -0,0 +1,97 @@
+/*
+* Implementation file for the rigid body force generators.
+*
+* Part of the Cyclone physics system.
+*
+* Copyright (c) Icosagon 2003. All Rights Reserved.
+*
+* This software is distributed under license. Use of this software
+* implies agreement with all terms and conditions of the accompanying
+* software license.
+*/
+
+#include "ForceGenerator.h"
+
+
+void ForceRegistry::updateForces(real duration)
+{
+	Registry::iterator i = registrations.begin();
+	for (; i != registrations.end(); i++)
+	{
+		i->fg->updateForce(i->body, duration);
+	}
+}
+
+void ForceRegistry::add(RigidBody *body, ForceGenerator *fg)
+{
+	ForceRegistry::ForceRegistration registration;
+	registration.body = body;
+	registration.fg = fg;
+	registrations.push_back(registration);
+}
+
+Aero::Aero(const Matrix3 &tensor, const Vector3 &position, const Vector3 *windspeed)
+{
+	Aero::tensor = tensor;
+	Aero::position = position;
+	Aero::windspeed = windspeed;
+}
+
+void Aero::updateForce(RigidBody *body, real duration)
+{
+	Aero::updateForceFromTensor(body, duration, tensor);
+}
+
+void Aero::updateForceFromTensor(RigidBody *body, real duration,
+	const Matrix3 &tensor)
+{
+	// Calculate total velocity (windspeed and body's velocity).
+	Vector3 velocity = body->getVelocity();
+	velocity += *windspeed;
+
+	// Calculate the velocity in body coordinates
+	Vector3 bodyVel = body->getTransform().transformInverseDirection(velocity);
+
+	// Calculate the force in body coordinates
+	Vector3 bodyForce = tensor.transform(bodyVel);
+	Vector3 force = body->getTransform().transformDirection(bodyForce);
+
+	// Apply the force
+	body->addForceAtBodyPoint(force, position);
+}
+
+AeroControl::AeroControl(const Matrix3 &base, const Matrix3 &min, const Matrix3 &max,
+	const Vector3 &position, const Vector3 *windspeed)
+	:
+	Aero(base, position, windspeed)
+{
+	AeroControl::minTensor = min;
+	AeroControl::maxTensor = max;
+	controlSetting = 0.0f;
+}
+
+Matrix3 AeroControl::getTensor()
+{
+	if (controlSetting <= -1.0f) return minTensor;
+	else if (controlSetting >= 1.0f) return maxTensor;
+	else if (controlSetting < 0)
+	{
+		return Matrix3::linearInterpolate(minTensor, tensor, controlSetting + 1.0f);
+	}
+	else if (controlSetting > 0)
+	{
+		return Matrix3::linearInterpolate(tensor, maxTensor, controlSetting);
+	}
+	else return tensor;
+}
+
+void AeroControl::setControl(real value)
+{
+	controlSetting = value;
+}
+
+void AeroControl::updateForce(RigidBody *body, real duration)
+{
+	Matrix3 tensor = getTensor();
+	Aero::updateForceFromTensor(body, duration, tensor);
+}
